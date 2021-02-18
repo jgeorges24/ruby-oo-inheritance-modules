09ec0edd30934f5262bd93b0680cccbbdfc1563c
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-18 11:00:06 -0500

    commit tree db60a3ff44655d7599c5f82eebd12cdae695811c

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,10 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-
+require_relative 'fancy_dance.rb'
 class Dancer
-    attr_accessor :name
-
+     attr_accessor :name
+     extend MetaDancing
+     extend FancyDance::ClassMethods
+     include FancyDance::InstanceMethods
     include Dance
+    
 
     def initialize(name)
         @name = name
